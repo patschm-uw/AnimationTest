fc9c962ff39eedf45b2780652c80611ee4ec0d14
Patrick Schmeichel <patschm@uw.edu>
2021-01-14 00:31:45 -0800

    Update character.js

--- character.js
+++ character.js
@@ -3,6 +3,13 @@ class Link {
 		Object.assign(this, {game, x, y});
 		
 		this.spritesheet = ASSET_MANAGER.getAsset("./Sprites/LinkWalk.png");
+		
+		this.animations = [];
+		this.loadAnimations();
+	};
+	
+	loadAnimations() {
+		this.animations[0] = new Animator(this.spritesheet, 100, 0, 10, 10, 1, 0.33, 14, false, true);
 	};
 	
 	update(){
