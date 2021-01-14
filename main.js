35dc294d96407d7a175971809ccf7835d17f9054
Patrick Schmeichel <patschm@uw.edu>
2021-01-13 23:38:00 -0800

    update

--- main.js
+++ main.js
@@ -2,9 +2,13 @@ var gameEngine = new GameEngine();
 
 var ASSET_MANAGER = new AssetManager();
 
+ASSET_MANAGER.queueDownload("./Sprites/Link.png");
+
 ASSET_MANAGER.downloadAll(function () {
 	var canvas = document.getElementById('gameWorld');
 	var ctx = canvas.getContext('2d');
+
+	
 
 	gameEngine.init(ctx);
 
