6eb391e11b8a129c01631bc982aaf04e0a9b66cc
Patrick Schmeichel <patschm@uw.edu>
2021-01-14 17:37:15 -0800

    Added new character animation

--- character.js
+++ character.js
@@ -4,10 +4,11 @@ class Link {
 		this.x = x;
 		this.y = y;
 		
-		this.spritesheet = ASSET_MANAGER.getAsset("./Sprites/LinkWalk.png");
+		this.spritesheet = ASSET_MANAGER.getAsset("./Sprites/Minish.png");
 		
 		this.animations = [];
-		this.animations.push(new Animator(this.spritesheet, 2, 32, 30, 30, 6, .18, 0, false, true));
+		this.animations.push(new Animator(this.spritesheet, 6, 1760, 32, 42, 7, .1, 0, false, true));
+		//                                                x, y, width, height, frames, speed, spacing, flip, loop
 	};
 	
 	update(){
@@ -15,7 +16,76 @@ class Link {
 	};
 	
 	draw(ctx) {
-		//ctx.drawImage(this.spritesheet,0,0,204*2,115*2);
+		//ctx.drawImage(this.spritesheet,0,0,1208,3735);
 		this.animations[0].drawFrame(this.game.clockTick, ctx, this.x, this.y, 3.5)
+	};
+};
+
+class Ezlo {
+	constructor(game,x,y) {
+		this.game = game;
+		this.x = x;
+		this.y = y;
+		
+		this.spritesheet = ASSET_MANAGER.getAsset("./Sprites/Minish.png");
+		
+		this.animations = [];
+		this.animations.push(new Animator(this.spritesheet, 2, 32, 67, 49, 6, .18, 0, false, true));
+		//                                                x, y, width, height, frames, speed, spacing, flip, loop
+	};
+	
+	update() {
+	
+	};
+	
+	draw(ctx) {
+		//ctx.drawImage(this.spritesheet, 0, 0, 100, 100);
+		this.animations[0].drawFrame(this.game.clockTick, ctx, this.x, this.y, 3.5);
+	};
+};
+
+class Arachnus {
+	constructor(game,x,y) {
+		this.game = game;
+		this.x = x;
+		this.y = y;
+		
+		this.spritesheet = ASSET_MANAGER.getAsset("./Sprites/Arachnus.png");
+		
+		this.animations = [];
+		this.animations.push(new Animator(this.spritesheet, 1, 670, 88, 78, 25, .1, 2, false, true));
+		//                                                x, y, width, height, frames, speed, spacing, flip, loop
+	};
+	
+	update() {
+	
+	};
+	
+	draw(ctx) {
+		//ctx.drawImage(this.spritesheet, 0, 0, 100, 100);
+		this.animations[0].drawFrame(this.game.clockTick, ctx, this.x, this.y, 3.5);
+	};
+};
+
+class Shell {
+	constructor(game,x,y) {
+		this.game = game;
+		this.x = x;
+		this.y = y;
+		
+		this.spritesheet = ASSET_MANAGER.getAsset("./Sprites/Arachnus.png");
+		
+		this.animations = [];
+		this.animations.push(new Animator(this.spritesheet, 0, 755, 90, 70, 25, .1, 0, false, true));
+		//                                                x, y, width, height, frames, speed, spacing, flip, loop
+	};
+	
+	update() {
+	
+	};
+	
+	draw(ctx) {
+		//ctx.drawImage(this.spritesheet, 0, 0, 100, 100);
+		this.animations[0].drawFrame(this.game.clockTick, ctx, this.x, this.y, 3.5);
 	};
 };
--- main.js
+++ main.js
@@ -2,13 +2,17 @@ var gameEngine = new GameEngine();
 
 var ASSET_MANAGER = new AssetManager();
 
-ASSET_MANAGER.queueDownload("./Sprites/LinkWalk.png");
+ASSET_MANAGER.queueDownload("./Sprites/Minish.png");
+ASSET_MANAGER.queueDownload("./Sprites/Arachnus.png");
 
 ASSET_MANAGER.downloadAll(function () {
 	var canvas = document.getElementById('gameWorld');
 	var ctx = canvas.getContext('2d');
 
 	gameEngine.init(ctx);
-	gameEngine.addEntity(new Link(gameEngine, 50, 50));
+	gameEngine.addEntity(new Shell(gameEngine, 45, 93));
+	gameEngine.addEntity(new Arachnus(gameEngine, 50, 50));
+	//gameEngine.addEntity(new Link(gameEngine, 600, 180));
+	//gameEngine.addEntity(new Ezlo(gameEngine, 50, 50));
 	gameEngine.start();
 });
